1752c6fae3c7c508597bf9578db440a35c838942
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-11 19:24:43 +0900

    [add]deviseを使ったログイン機能を一般ユーザーと管理者に分けて実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
