c54252216031bee980ad0229aac8943f76cb94ea
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-11 22:06:39 +0900

    [fix]partが作成できないエラーを修正

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,4 +1,6 @@
 class PartsController < ApplicationController
+  before_action :set_part, only: [:show, :edit, :update, :destroy]
+
   def index
     @parts=Part.all
   end
@@ -8,10 +10,9 @@ class PartsController < ApplicationController
   end
 
   def create
-    @part=current_user.parts.build(part_params)
+    @part = Part.new(part_params)
 
     if @part.save
-      PartMailer.part_mail(@part).deliver
       redirect_to parts_path, notice: "投稿しました"
     else
       render 'new'
@@ -19,19 +20,12 @@ class PartsController < ApplicationController
   end
 
   def show
-    @part=Part.find(params[:id])
   end
 
   def edit
-    @part=Part.find(params[:id])
-    if @part.user_id!=current_user&.id
-      redirect_to root_path
-    end
   end
 
   def update
-    @part=Part.find(params[:id])
-
     if @part.update(part_params)
       redirect_to parts_path, notice: "投稿しました"
     else
@@ -40,12 +34,15 @@ class PartsController < ApplicationController
   end
 
   def destroy
-    @part=Part.find(params[:id])
     @part.destroy
     redirect_to parts_path, notice: '削除しました'
   end
 
   private
+
+  def set_part
+    @part = Part.find(params[:id])
+  end  
 
   def part_params
     params.require(:part).permit(:title, :content, :status)
