543c5d6097895782531bf068a723ec59cac9ae36
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-11 19:49:46 +0900

    [edit]deviseでユーザーの名前が登録できるよう編集

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_093024) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_093024) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
