17a9848b0a6008e2b98c6a57c0eda9b70fe19c05
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-25 04:06:05 +0900

    [add]awsのsesの設定を追加し、deviseのメール機能が使用できるよう設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+  config.action_mailer.default_url_options = { host: 'quick-learning-app.com' }
+  config.action_mailer.delivery_method = :ses
   #以下sendgridの設定 config.action_mailer.default_url_options = { host: 'guarded-caverns-14017.herokuapp.com' }
   # ActionMailer::Base.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
