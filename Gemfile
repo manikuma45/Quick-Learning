f082c879d2503067e8d67cfe742e071646b6230e
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-25 03:22:13 +0900

    [add]awsのsesの設定を追加し、deviseのメール機能が使用できるよう設定

--- Gemfile
+++ Gemfile
@@ -82,3 +82,4 @@ gem 'unicorn'
 gem 'mini_racer', platforms: :ruby
 gem 'ed25519'
 gem 'bcrypt_pbkdf'
+gem 'aws-ses'
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'Quick-learning-app <noreply@quick-learning-app.com>'
   layout 'mailer'
 end
