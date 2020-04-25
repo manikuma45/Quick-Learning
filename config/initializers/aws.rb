c94db1aa55b443e056b245bddcd233a3351dd76c
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-04-25 10:37:16 +0900

    [add]awsのsesの設定を追加し、deviseのメール機能が使用できるよう設定

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,5 +3,5 @@ ActionMailer::Base.add_delivery_method(
   AWS::SES::Base,
   access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
   secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-  server: 'email.us-west-2.amazonaws.com'
+  server: 'email.ap-southeast-2.amazonses.com'
 )
